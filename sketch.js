7de3e8874651bd002c3baa607e2a56baf127c506
andyk2022 <90669996+andyk2022@users.noreply.github.com>
2022-06-01 14:49:47 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,33 +5,32 @@ var endPos; //last position needed to be tested
 
 
 function setup() {
-  createCanvas(400, 400);
-  background(0);
-  arr = randomArray(10);
+  createCanvas(int(400), int(400));
+  background(int(0));
+  arr = randomArray(int(10));
 
-  pos = 0;
-  testPos = pos + 1;
-  endPos = arr.length - 1;
+  pos = int(0);
+  testPos = pos + int(1);
+  endPos = arr.length - int(1);
 }
 
 function draw() {
-  background(0);
-  
-  if (endPos==0) {
-    displayArray(arr, -1, -1, -1);
-  }
-  else {
+  background('OOOOOO');
+
+  if (endPos == int(0)) {
+    displayArray(arr, int(-1), int(-1), int(-1));
+  } else {
     displayArray(arr, pos, testPos, endPos);
-    if (arr[pos]>arr[testPos]) {
+    if (arr[pos] > arr[testPos]) {
       swap(arr, pos, testPos);
     } else {
       pos++;
       testPos++;
     }
-    
-    if (pos==endPos) {
-      pos=0;
-      testPos=1;
+
+    if (pos == endPos) {
+      pos = int(0);
+      testPos = int(1);
       endPos--;
     }
   }
@@ -40,32 +39,29 @@ function draw() {
 function randomArray(int num) {
   var values = new int[num];
 
-  for (int i=0; i<values.length; i++) {
-    values[i] = int(random(100, 400));
-  }//random value loop
+  for (int i = int(0); i < values.length; i++) {
+    values[i] = int(random(int(100), int(400)));
+  } //random value loop
   return values;
-}//randomArray
+} //randomArray
 
 function displayArray(arr, int p, int tp, int sp) {
   var barWidth = width / arr.length;
   var x = 0;
   var y = 0;
   noStroke();
-  for (int i=0; i<arr.length; i++) {
+  for (int i = 0; i < arr.length; i++) {
     y = height - arr[i];
-    if (i == p ) {
+    if (i == p) {
       fill(230, 0, 230);
-    }
-    else if (i == tp) {
+    } else if (i == tp) {
       fill(0, 230, 230);
-    }
-    else if (i == sp) {
+    } else if (i == sp) {
       fill(230, 230, 0);
-    }
-    else {
+    } else {
       fill(255);
     }
     rect(x, y, barWidth, arr[i]);
-    x+= barWidth;
+    x += barWidth;
   }
-}//displayArray
+} //displayArray
