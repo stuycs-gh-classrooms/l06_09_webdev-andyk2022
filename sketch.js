ca535b3100ff754fc425489cf377bfda9d4970ae
andyk2022 <90669996+andyk2022@users.noreply.github.com>
2022-06-03 00:15:46 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,61 +1,69 @@
-function setup() {
-  createCanvas(400, 400);
+var xspeed;
+var yspeed;
+var x;
+var y;
+var heightToBorder;
+var widthToBorder;
+var xspeed1;
+var yspeed1;
+var x1;
+var y1;
+
+function setup(){ 
   background(0);
-  const arr = new Array(10);
-
-  var pos = 0;
-  var testPos = pos + 1;
-  var endPos = arr.length - 1;
+  createCanvas(400,400);
+  xspeed=0;
+  while (xspeed<=2 && xspeed>=-2){
+    xspeed=int(random(-5,6));
+  }
+  yspeed=0;
+  while (yspeed<=2 && yspeed>=-2){
+    yspeed=int(random(-5,6));
+  }
+  x=int(random(5,395-widthToBorder));
+  y=int(random(5,395-heightToBorder));
+  heightToBorder=25;
+  widthToBorder=50;
+  
+  xspeed1=0;
+  while (xspeed1<=2 && xspeed1>=-2){
+    xspeed1=int(random(-5,6));
+  }
+  yspeed1=0;
+  while (yspeed1<=2 && yspeed1>=-2){
+    yspeed1=int(random(-5,6));
+  }
+  x1=int(random(5,395-widthToBorder));
+  y1=int(random(5,395-heightToBorder));
 }
 
-function draw() {
-  background('OOOOOO');
-
-  if (endPos == 0) {
-    displayArray(arr, -1, -1), -1);
-  } else {
-    displayArray(arr, pos, testPos, endPos);
-    if (arr[pos] > arr[testPos]) {
-      swap(arr, pos, testPos);
-    } else {
-      pos++;
-      testPos++;
-    }
-
-    if (pos == endPos) {
-      pos = 0;
-      testPos = 1;
-      endPos--;
-    }
+function draw(){
+  background(0);
+  x+=xspeed;
+  y+=yspeed;
+  if ((x+2*widthToBorder)>=width){
+    xspeed=int(random(-5,-2));
+  }else if (x<0){
+    xspeed=int(random(3,6));
   }
+  if ((y+2*heightToBorder)>=height){
+    yspeed=int(random(-5,-2));
+  }else if (y<0){
+    yspeed=int(random(3,6));
+  }
+  rect(x,y,2*widthToBorder,2*heightToBorder);
+  
+  x1+=xspeed1;
+  y1+=yspeed1;
+  if ((x1+2*widthToBorder)>=width){
+    xspeed1=int(random(-5,-2));
+  }else if (x1<0){
+    xspeed1=int(random(3,6));
+  }
+  if ((y1+2*heightToBorder)>=height){
+    yspeed1=int(random(-5,-2));
+  }else if (y1<0){
+    yspeed1=int(random(3,6));
+  }
+  rect(x1,y1,2*widthToBorder,2*heightToBorder);
 }
-
-function randomArray(num) {
-  const value = new Array(num)
-
-  for (let i = 0; i < values.length; i++) {
-    values[i] = int(random(100, 400));
-  } //random value loop
-  return values;
-} //randomArray
-
-function displayArray(arr, p, tp, sp) {
-  var barWidth = width / arr.length;
-  var x = 0;
-  var y = 0;
-  noStroke();
-  for (let i = 0; i < arr.length; i++) {
-    y = height - arr[i];
-    if (i == p) {
-      fill(230, 0, 230);
-    } else if (i == tp) {
-      fill(0, 230, 230);
-    } else if (i == sp) {
-      fill(230, 230, 0);
-    } else {
-      fill(255);
-    }
-    rect(x, y, barWidth, arr[i]);
-    x += barWidth;
-  }
-} //displayArray
